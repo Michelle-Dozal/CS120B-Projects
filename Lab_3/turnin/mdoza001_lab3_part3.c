1d70a78355a905c254848e30f0429a026ab82adc
Michelle Dozal <mdoza001@wch136-20.cs.ucr.edu>
2020-04-09 16:04:33 -0700

    Part 3

---
+++ Lab_3/turnin/mdoza001_lab3_part3.c
@@ -0,0 +1,52 @@
+/*	Author: mdoza001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #3  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+    /* Insert your solution below */
+	unsigned char input;
+	unsigned char fuel;
+	unsigned char per = PINA & 0x20;
+	unsigned char igni= PINA & 0x10;
+	unsigned char beltl = PINA & 0x40;	
+   	input = PINA;
+	 while (1) {
+        if((input & 0x00) == 0x00){fuel = 0x00;}
+	 if((input& 0x01) == 0x01){fuel = 0x60;}
+	  if((input & 0x02) == 0x02){fuel = 0x60;}
+	  if((input & 0x03) == 0x03){fuel = 0x70;}
+	  if((input & 0x04) == 0x04){fuel = 0x70;}
+	  if((input & 0x05) == 0x05){fuel = 0x38;}
+	  if((input & 0x06) == 0x06){fuel = 0x38;}
+	  if((input & 0x07) == 0x07){fuel = 0x3C;}
+	  if((input & 0x08) == 0x08){fuel = 0x3C;}
+	  if((input &  0x09) == 0x09){fuel = 0x3C;}
+	  if((input &  0x0A) == 0x0A){fuel = 0x3E;}
+	  if((input & 0x0B) == 0x0B){fuel = 0x3E;}
+	  if((input &  0x0C) == 0x0C){fuel = 0x3E;}
+	  if((input & 0x0D)  == 0x0D){fuel = 0x3F;}
+	  if((input &  0x0E) == 0x0E){fuel = 0x3F;}
+	  if((input &  0x0F) == 0x0F){fuel = 0x3F;}
+		
+	if(per == 0x20){
+	if (igni == 0x10){
+		if(beltl == 0x40){
+			PORTC = fuel;}
+		else {PORTC = fuel | 0xA0;}
+	}
+    }
+   PORTC = fuel;
+}	return 1; }
